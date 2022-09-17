7480db73add6ec90f2b60f2158680ed0cb7a8e30
guoanhu <8naouguh@gmail.com>
2022-09-17 11:09:26 +0800

    first youtube home page customize script

---
+++ youtube-home-page-only-search.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+            document.querySelectorAll("#primary").forEach(el => {
+  el.remove();
+});
+})();
